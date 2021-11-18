535f773d5ebe177cbcde52e3aaebbc42eaf5949f
viktordw <viktor@dreamweb.rs>
2021-11-18 09:21:43 +0100

    Nacrt

--- mreport.sh
+++ mreport.sh
@@ -26,3 +26,99 @@ get_value() {
     shift
     done
 }
+
+fn_output_coloring_off () {
+  RED=''
+  GREEN=''
+  YELLOW=''
+  BLACK=''
+  WHITE=''
+  NC=''
+  BGREEN=''
+  BGRAY=''
+  BNC=''
+}
+
+fn_output_coloring_on () {
+  # Text colors
+  RED='\033[0;31m'
+  GREEN='\033[0;32m'
+  YELLOW='\033[1;33m'
+  BLACK='\e[30m'
+  WHITE='\e[97m'
+
+  # Text color reset
+  NC='\033[0m'
+
+  # Background color
+  BGREEN='\e[42m'
+  BGRAY='\e[47m'
+
+  # Background color reset
+  BNC='\e[49m'
+}
+
+# ################################################# #
+# KONFIGURACIJA                                     #
+# ################################################# #
+
+conf_report_file='mreport.txt'
+
+
+# ################################################# #
+
+# Read arguments
+if [ "$#" = 0 ]; then
+  usage
+  exit 1
+else
+  user=$(get_value "user" "$@")
+  user=$(get_value "u" "$@")
+  domain=$(get_value "domain" "$@")
+  domain=$(get_value "d" "$@")
+  ip=$(get_value "ip" "$@")
+  mlist=$(get_value "mlist" "$@")
+  ll=$(get_value "ll" "$@")
+fi
+
+# ################################################# #
+# FUNKCIJE                                          #
+# ################################################# #
+skip () {
+  printf ''
+}
+generate_dir () {
+
+}
+# ################################################# #
+
+if [ -n "$mlist" ]; then
+  echo "Retrieving malware list..."
+  sudo dwh -u $user --mlist | head -5 |
+else
+  skip
+fi
+
+if [ -n "$ll" ]; then
+  echo "Retrieving list from Last Login file..."
+else
+  skip
+fi
+
+if [ -n "$user" ]; then
+  echo "Finding user details..."
+else
+  skip
+fi
+
+if [ -n "$domain" ]; then
+  echo "Finding domain details..."
+else
+  skip
+fi
+
+if [ -n "$IP" ]; then
+  echo "Finding IP address details..."
+else
+  skip
+fi
