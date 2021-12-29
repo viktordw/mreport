b3d970cda50b0905c10d2c86d8b188501caf06bc
viktordw <viktor@dreamweb.rs>
2021-12-29 13:10:16 +0100

    Premešten glavni deo koda u main.sh

--- mreport.sh
+++ mreport.sh
@@ -26,45 +26,12 @@ else
   ll=$(get_value "ll" "$@")
 fi
 
+
+
 # ######################################### #
 make_data_dir
 make_report_dir
 
 data_path="${conf_data_dirname}${report_dir}"
 
-if [ "$mr_dir" = 'true' ]; then
-  skip
-else
-  skip
-fi
-
-if [ -n "$mlist" ]; then
-  echo "Retrieving malware list..."
-  sudo dwh -u "$user" --mlist > /mlist.txt
-else
-  skip
-fi
-
-if [ -n "$ll" ]; then
-  echo "Retrieving list from Last Login file..."
-else
-  skip
-fi
-
-if [ -n "$user" ]; then
-  echo "Finding user details..."
-else
-  skip
-fi
-
-if [ -n "$domain" ]; then
-  echo "Finding domain details..."
-else
-  skip
-fi
-
-if [ -n "$IP" ]; then
-  echo "Finding IP address details..."
-else
-  skip
-fi
+fn_main
